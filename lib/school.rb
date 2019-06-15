b47f6302177d27a2688d565cfa2767cc1205fa48
Jack Atherton <jackrrye@gmail.com>
2019-06-15 16:37:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+require "pry"
+
+class School
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  @@roster = {}
+  
+  def roster
+    @@roster
+  end
+  
+  def add_student(student, grade)
+    @@roster[grade] = grade
+
+  end
+  
+end
